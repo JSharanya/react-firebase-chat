08013edfe246e245bc6cbfdae8cd84d4406b0846
Sharanya Jegadeesan <sharanyajegadeesan1999@gmail.com>
2024-06-26 11:04:45 +0530

    adduser componets

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import './chatList.css'
+import AddUser from '../../addUser/addUser'
+
 
 const ChatList = () => {
 
@@ -63,7 +65,7 @@ const ChatList = () => {
           <p>Hello</p>
         </div>
       </div>
-
+      {addMode && <AddUser/>}
     </div>
   )
 }
